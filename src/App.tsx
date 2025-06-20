99d1e811adb35a855059598a393dbd340bff155c
Pedro Vidal <pedrorvidal@gmail.com>
2025-06-20 19:34:47 -0300

    basic css for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
-
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
-    <div>
-      <Heading>
-        Olá mundo from app 1
-        <button>
-          <TimerIcon size={24} />
-        </button>
-      </Heading>
-      <Heading>Olá mundo from app 2</Heading>
-      <Heading>Olá mundo from app 3</Heading>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi,
-        pariatur temporibus eius libero reprehenderit iure dolorum nihil facilis
-        quas praesentium in obcaecati sequi illo. Ipsum cumque incidunt aliquam
-        eveniet dolore.
-      </p>
-    </div>
+    <>
+      <div className="container">
+        <div className="content">
+          <section>
+            <p>LOGO</p>
+          </section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>
+            <p>MENU</p>
+          </section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>
+            <p>FORM</p>
+          </section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>
+            <p>FOOTER</p>
+          </section>
+        </div>
+      </div>
+    </>
   );
 }
