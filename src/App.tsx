da52e6a7e77d9375100ecaacf83aff4712365fb8
Pedro Vidal <pedrorvidal@gmail.com>
2025-06-21 12:51:57 -0300

    building container modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,21 @@
+import { Container } from './components/Container';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <section>
-            <p>LOGO</p>
-          </section>
-        </div>
-      </div>
+      <Container>
+        <section>
+          <p>LOGO</p>
+        </section>
+      </Container>
 
-      <div className="container">
-        <div className="content">
-          <section>
-            <p>MENU</p>
-          </section>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="content">
-          <section>
-            <p>FORM</p>
-          </section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>
-            <p>FOOTER</p>
-          </section>
-        </div>
-      </div>
+      <Container>
+        <section>
+          <p>MENU</p>
+        </section>
+      </Container>
     </>
   );
 }
