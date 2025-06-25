6b447c413413e194d4668c6053f080e868a1af41
Pedro Vidal <pedrorvidal@gmail.com>
2025-06-24 23:47:38 -0300

    countdown component styles

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,13 +1,5 @@
-import { TimerIcon } from 'lucide-react';
 import styles from './styles.module.css';
 
 export function Countdown() {
-  return (
-    <div className={styles.countdown}>
-      <a className={styles.countdownLink} href="#">
-        <TimerIcon className={styles.icon} />
-        <span>Chronos</span>
-      </a>
-    </div>
-  );
+  return <div className={styles.container}>00:00</div>;
 }
