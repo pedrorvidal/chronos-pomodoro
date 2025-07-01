1546a7faac519f5aabcdf690d9f3d7a62fc4d3eb
Pedro Vidal <pedrorvidal@gmail.com>
2025-06-30 23:52:58 -0300

    cycles component

---
+++ src/components/Cycles/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css';
+
+export function Cycles() {
+  return (
+    <div className={styles.cycles}>
+      <h1 className={styles.title}>Cycles Component</h1>
+      <p className={styles.description}>This is the Cycles component.</p>
+    </div>
+  );
+}
