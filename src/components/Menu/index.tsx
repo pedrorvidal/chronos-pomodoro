905b80f795190096f6647fe237558b9ada394199
Pedro Vidal <pedrorvidal@gmail.com>
2025-07-06 20:31:27 -0300

    useEffect to control page theme

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,24 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
   const [theme, setTheme] = useState<AvailableThemes>('dark');
   function handleThemeChange(event) {
-    event?.preventDefault();
-    console.log('Theme changed', theme);
-    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+    event.preventDefault();
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
   }
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
 
   return (
     <nav className={styles.menu}>
-      <h1>{theme}</h1>
       <a className={styles.menuLink} href="#" aria-label="Home" title="Home">
         <HouseIcon className={styles.icon} />
       </a>
