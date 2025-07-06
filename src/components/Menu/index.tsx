983ed4aae9c6715714a796d1b5db32051f8520c5
Pedro Vidal <pedrorvidal@gmail.com>
2025-07-05 22:15:53 -0300

    html lang + theme attr + menu accessibility

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,16 +4,26 @@ import styles from './styles.module.css';
 export function Menu() {
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href="#" aria-label="Home" title="Home">
         <HouseIcon className={styles.icon} />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Histórico"
+        title="Histórico"
+      >
         <HistoryIcon className={styles.icon} />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Configurações"
+        title="Configurações"
+      >
         <SettingsIcon className={styles.icon} />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href="#" aria-label="Tema" title="Tema">
         <SunIcon className={styles.icon} />
       </a>
     </nav>
