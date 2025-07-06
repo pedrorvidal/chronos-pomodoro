ee8cc35df5d7fd55dedeffd9f7b00ae520d1cf39
Pedro Vidal <pedrorvidal@gmail.com>
2025-07-05 22:25:36 -0300

    adding state to theme button

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,20 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+  function handleThemeChange(event) {
+    event?.preventDefault();
+    console.log('Theme changed', theme);
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+  }
+
   return (
     <nav className={styles.menu}>
+      <h1>{theme}</h1>
       <a className={styles.menuLink} href="#" aria-label="Home" title="Home">
         <HouseIcon className={styles.icon} />
       </a>
@@ -23,7 +34,13 @@ export function Menu() {
       >
         <SettingsIcon className={styles.icon} />
       </a>
-      <a className={styles.menuLink} href="#" aria-label="Tema" title="Tema">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Tema"
+        title="Tema"
+        onClick={handleThemeChange}
+      >
         <SunIcon className={styles.icon} />
       </a>
     </nav>
