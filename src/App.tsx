277fc05e0a85dca44687fa807b88b834076fe6a9
Pedro Vidal <pedrorvidal@gmail.com>
2025-07-10 23:02:09 -0300

    starting with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import { AboutPomodoro } from './pages/AboutPomodoro';
 import { Home } from './pages/Home';
 import { NotFound } from './pages/NotFound';
 
 import './styles/global.css';
 import './styles/theme.css';
+import type { TaskStateModel } from './models/TaskStateModel';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+
+  return <Home state={state} setState={setState} />;
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,15 @@
 import { Container } from '../../components/Container';
 import { Countdown } from '../../components/CountDown';
 import { MainForm } from '../../components/MainForm';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 import { MainTemplate } from '../../templates/MainTemplate';
 
-export function Home() {
+type homeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+export function Home(props: homeProps) {
+  console.log(props);
   return (
     <>
       <MainTemplate>
